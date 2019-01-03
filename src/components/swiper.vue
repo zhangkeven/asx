b1bf7f50723aa003038e2e810df3171e5b7d1021
zhangkaiwen <193498162@qq.com>
2019-01-03 15:37:26 +0800

    update

---
+++ src/components/swiper.vue
@@ -0,0 +1,98 @@
+<template>
+	<div class="swiper-main">
+		<div class="swiper-mycontainer">
+			<div class="swiper-wrapper">
+				<!--<div class="swiper-slide" ><a href=""><img class="bannerImg" src="../assets/img/banner.png" alt=""></a></div>-->
+				<div class="swiper-slide" v-for="(item,index) in imgs" :key="index">
+					<img class="swiperImg" :src="item.imgUrl?item.imgUrl:''" alt="">
+				</div>
+			</div>
+			<!-- 如果需要分页器 -->
+			<div class="swiper-mypagination"></div>
+
+			<!-- 如果需要导航按钮 -->
+			<!--<div class="swiper-button-prev swiper-button-white"></div>-->
+			<!--<div class="swiper-button-next swiper-button-white"></div>-->
+		</div>
+	</div>
+</template>
+
+<script>
+	import Swiper from 'swiper'
+	export default {
+		data() {
+			return {
+				imgs:[]
+			};
+		},
+		props: {
+			bannerList: Array,
+		},
+		mounted() {
+			if(this.bannerList.length>0){
+				this.imgs = this.bannerList;
+			}
+			
+				new Swiper('.swiper-mycontainer', {
+					autoplay: 2500,
+					speed: 300,
+					autoplayDisableOnInteraction: false,
+					loopAdditionalSlides: 1,
+					loopedSlides: 0,
+					paginationClickable: true,
+					touchReleaseOnEdges: true,
+					loop: true,
+					observer: true, //修改图片后，自动初始化
+					observeParents: true,
+					observeSlideChildren:true,
+					// 如果需要分页器
+					pagination: '.swiper-mypagination',
+					// paginationHide:true,
+					// grabCursor:true,
+					// paginationVisibleClass:'my-visible-switch',
+					// 如果需要前进后退按钮
+					// nextButton: '.swiper-button-next',
+					// prevButton: '.swiper-button-prev',
+					// 如果需要滚动条
+					// scrollbar: '.swiper-scrollbar',
+				});
+			
+		},
+	}
+</script>
+
+<style>
+	.swiper-main {
+		width: 100%;
+	}
+
+	.swiper-mycontainer {
+		margin-left: auto;
+		margin-right: auto;
+		position: relative;
+		overflow: hidden;
+		/* Fix of Webkit flickering */
+		z-index: 1;
+		width: 100%;
+		
+	}
+
+	.swiperImg {
+		width: 100%;
+		cursor: pointer;
+	}
+
+	.swiper-mypagination{
+		position: absolute;
+		text-align: center;
+		-webkit-transition: 300ms;
+		-moz-transition: 300ms;
+		-o-transition: 300ms;
+		transition: 300ms;
+		-webkit-transform: translate3d(0, 0, 0);
+		-ms-transform: translate3d(0, 0, 0);
+		-o-transform: translate3d(0, 0, 0);
+		transform: translate3d(0, 0, 0);
+		z-index: 10;
+	}
+</style>
---
+++ src/components/topnav.vue
@@ -0,0 +1,168 @@
+<template>
+	<div class="main">
+		<div class="top-nav-bg">
+			<div class="topLeft">
+				<div class="topLeftTitle">
+					<img src="../assets/img/logo.png" alt="">
+				</div>
+			</div>
+			<div class="nav">
+				<a href="javascript:">首页</a>
+				<a href="javascript:">明星产品</a>
+				<a href="javascript:" @click="goCoreyroom()">料理教室</a>
+				<a href="javascript:" @click="goshare()">使用者分享</a>
+				<a href="javascript:" @click="goAbout()">关于我们</a>
+				<p href="javascript:">我要购买</p>
+			</div>
+			<div class="navRight">
+				<div class="searchBg">
+					<input type="text" @input="search" ref="inputVal" @keyup.enter="submit()">
+					<img src="../assets/img/search.png" alt="">
+				</div>
+				<img src="../assets/img/person.png" alt="">
+				<img src="../assets/img/cart.png" alt="">
+			</div>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				inputVal: '',
+				searchText: ''
+			};
+		},
+		methods: {
+			goMain() {
+				this.$router.push({
+					name: 'Main',
+					params: {}
+				})
+			},
+			goCoreyroom() {
+				this.$router.push({
+					name: 'Coreyroom',
+					params: {}
+				})
+			},
+			goshare() {
+				this.$router.push({
+					name: 'Share',
+					params: {}
+				})
+			},
+			goAbout() {
+				this.$router.push({
+					name: 'About',
+					params: {}
+				})
+			},
+			/*模糊搜索*/
+			search() {
+				console.log('input输入的值', this.$refs.inputVal.value);
+				this.searchText = this.$refs.inputVal.value
+			},
+			submit() {
+				// 子组件中触发父组件方法searchInput并传值
+				this.$emit('listenChildEvent', this.searchText)
+			}
+		}
+	}
+</script>
+
+<style>
+	.top-nav-bg {
+		width: 70.5%;
+		min-width: 72rem;
+		background-color: #ffffff;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem 2%;
+		position: fixed;
+		top: 3.5rem;
+		z-index: 4;
+		box-shadow: 0 0.3125rem 0.9375rem 0 #ececec;
+	}
+
+	.topLeft {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		cursor: pointer;
+	}
+
+	.nav {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		width: 50%;
+	}
+
+	.nav a {
+		font-size: 1rem;
+		transition: color linear .5s;
+	}
+
+	.nav a:hover,
+	.nav1 a:hover {
+		color: #9f303d;
+	}
+
+	.nav p {
+		width: 5rem;
+		height: 2.5rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #852833;
+		color: #ffffff;
+		border-radius: 0.5rem;
+		cursor: pointer;
+		font-size: 1rem;
+	}
+
+	.navRight {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		flex-wrap: nowrap;
+		align-items: center;
+	}
+
+	.searchBg {
+		background-color: #EEE;
+		padding: 0.5rem 0.5rem;
+		width: 13rem;
+		height: 1rem;
+		border-radius: 1rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		margin-right: 1.5rem;
+	}
+
+	.searchBg input {
+		border: none;
+		width: 80%;
+		background-color: #EEE;
+		flex: 1;
+		outline: none;
+	}
+
+	.searchBg img {
+		cursor: pointer;
+	}
+
+	.navRight>img {
+		margin-right: 1.5rem;
+		width: 1rem;
+		cursor: pointer;
+	}
+</style>
